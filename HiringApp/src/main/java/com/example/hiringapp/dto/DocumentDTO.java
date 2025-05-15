c2ae80dc6c939e4adeb22a23c71176382284fd6a
ayushiraturi016@gmail.com <ayushiraturi016@gmail.com>
2025-05-15 12:52:49 +0530

    [Ayushi] add: DocumentDTO.java

---
+++ HiringApp/src/main/java/com/example/hiringapp/dto/DocumentDTO.java
@@ -0,0 +1,19 @@
+package com.example.hiringapp.dto;
+
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class DocumentDTO {
+    @NotNull(message = "Candidate ID is required")
+    private Long candidateId;
+    @NotNull(message = "Document type is required")
+    private String documentType;
+}
+
