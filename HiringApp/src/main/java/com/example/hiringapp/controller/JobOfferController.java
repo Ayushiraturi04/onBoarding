73e8cb6727fbbe4e5b73384be69bbdc61e5e49fc
ayushiraturi016@gmail.com <ayushiraturi016@gmail.com>
2025-05-15 12:45:51 +0530

    [Ayushi] add: JobOfferController.java

---
+++ HiringApp/src/main/java/com/example/hiringapp/controller/JobOfferController.java
@@ -0,0 +1,23 @@
+package com.example.hiringapp.controller;
+
+
+
+import com.example.hiringapp.service.JobOfferService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/candidates")
+public class JobOfferController {
+    @Autowired
+    private JobOfferService jobOfferService;
+
+    @PostMapping("/{id}/job-notify")
+    public ResponseEntity<Void> getJobOfferService(@PathVariable Long id,
+                                                   @RequestParam String position) {
+        jobOfferService.notify(id, position);
+        return ResponseEntity.ok().build();
+
+    }
+}
